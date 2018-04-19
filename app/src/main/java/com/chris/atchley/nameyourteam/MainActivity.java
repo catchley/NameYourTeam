6e9136867a5ad2bad1de28e438bf6fec249115af
Chris Atchley <steve.chris.atchley@gmail.com>
2018-04-19 19:15:53 -0400

    Added In-app purchase to remove ads.

--- app/src/main/java/com/chris/atchley/nameyourteam/MainActivity.java
+++ app/src/main/java/com/chris/atchley/nameyourteam/MainActivity.java
@@ -1,6 +1,9 @@
 package com.chris.atchley.nameyourteam;
 
+import android.content.Intent;
 import android.content.res.Resources;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,33 +11,44 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.anjlab.android.iab.v3.BillingProcessor;
+import com.anjlab.android.iab.v3.TransactionDetails;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.InterstitialAd;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BillingProcessor.IBillingHandler{
 
     Button mTwoWordObjectButton;
     Button mThreeWordObjectButton;
+    Button mRemoveAdButton;
     TextView mNameTextView;
     CheckBox mColorCheckBox;
     CheckBox mAnimalCheckBox;
     String firstWord;
     String secondWord;
+
     String thirdWord;
     String color;
 
+
     int adCounter = 0;
+    boolean adsOn = true;
     private InterstitialAd mInterstitialAd;
+
+    BillingProcessor bp;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bp = new BillingProcessor(this, null, this);
         mInterstitialAd = new InterstitialAd(this);
         mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
         mInterstitialAd.loadAd(new AdRequest.Builder().build());
@@ -59,11 +73,20 @@ public class MainActivity extends AppCompatActivity {
 
         mTwoWordObjectButton = (Button) findViewById(R.id.twoWordObject);
         mThreeWordObjectButton = (Button) findViewById(R.id.threeWordObject);
+        mRemoveAdButton = (Button)findViewById(R.id.removeAdButton);
         mNameTextView = (TextView) findViewById(R.id.nameTextView);
         mColorCheckBox= (CheckBox)findViewById(R.id.colerCheckBox);
         mAnimalCheckBox= (CheckBox)findViewById(R.id.animalCheckBox);
 
         mNameTextView.setText("Team Name");
+
+        mRemoveAdButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bp.purchase(MainActivity.this,"android.test.purchased");
+
+            }
+        });
 
 
 
@@ -72,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                if (adCounter >= 15) {
+                if ((adCounter >= 15) &&(adsOn == true)){
                     if (mInterstitialAd.isLoaded()) {
                         mInterstitialAd.show();
                         adCounter = 0;
@@ -106,6 +129,16 @@ public class MainActivity extends AppCompatActivity {
         mThreeWordObjectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                if ((adCounter >= 15) &&(adsOn == true)){
+                    if (mInterstitialAd.isLoaded()) {
+                        mInterstitialAd.show();
+                        adCounter = 0;
+                    } else {
+                        Log.d("TAG", "The interstitial wasn't loaded yet.");
+                    }
+                }
+
                 if(mAnimalCheckBox.isChecked()) {
                     firstWord = adjectives[new Random().nextInt(adjectives.length)];
                     secondWord = adjectives[new Random().nextInt(adjectives.length)];
@@ -130,5 +163,47 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    public void onProductPurchased(@NonNull String productId, @Nullable TransactionDetails details) {
+
+
+
+
+        Toast.makeText(this, "Purchase Success", Toast.LENGTH_SHORT).show();
+        adsOn = false;
+        mRemoveAdButton.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void onPurchaseHistoryRestored() {
+
+    }
+
+    @Override
+    public void onBillingError(int errorCode, @Nullable Throwable error) {
+
+        Toast.makeText(this, "Something went wrong.", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onBillingInitialized() {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!bp.handleActivityResult(requestCode, resultCode, data)) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        if (bp != null) {
+            bp.release();
+        }
+        super.onDestroy();
     }
 }
